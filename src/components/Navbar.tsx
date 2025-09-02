cc3ff5ee93867511cc8c9eb72d31d9160ba8636c
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-09-03 00:15:59 +0600

    btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ThemeToggleButton from './ui/theme-toggle-button'
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const Navbar = () => {
   return (
@@ -10,10 +11,18 @@ const Navbar = () => {
          ">StoryNest</h1>
          <div className='bg-[#F97316] h-2 w-2 rounded-full mt-4' ></div>
       </div>
-   
-        <div className="mt-6 cursor-pointer">
-        <ThemeToggleButton/>
-      </div>
+     
+
+ <div className="flex items-center gap-4 font-sans">
+  <button className='flex gap-2 items-center bg-orange-600 px-4 py-2 rounded-4xl'>
+   <IoIosAddCircleOutline className=" text-2xl" />
+  <span>Add Story</span>
+  </button>
+
+  <div className="cursor-pointer">
+    <ThemeToggleButton />
+  </div>
+</div>
      
 
    </nav>
